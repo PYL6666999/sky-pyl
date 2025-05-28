fa98147326305eec4d6ac0b97663071474852a4e
a999 <1481249965@qq.com>
2025-05-28 09:55:20 +0800

    初始化工程

---
+++ demo/main/java/org/example/skypyl/SkyPylApplication.java
@@ -0,0 +1,13 @@
+package org.example.skypyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyPylApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyPylApplication.class, args);
+    }
+
+}
---
+++ sky-pojo/src/test/java/org/example/skypojo/SkyPojoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.skypojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyPojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
